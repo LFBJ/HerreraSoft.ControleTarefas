186e6894809ea92df7a829741f2ea57a39bdbf62
Diego Herrera <di.herrera.go@gmail.com>
2018-10-29 23:43:42 -0300

    Criacao Estrutura

---
+++ HerreraSoft.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HerreraSoft.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ HerreraSoft.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HerreraSoft.Infra
+{
+    public class Class1
+    {
+    }
+}
