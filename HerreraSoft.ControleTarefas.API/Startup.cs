c37781a997b46fadfba9b1529739cc2f851d8632
Diego Herrera <di.herrera.go@gmail.com>
2018-10-29 23:13:54 -0300

    Teste 2

--- HerreraSoft.ControleTarefas.API/Startup.cs
+++ HerreraSoft.ControleTarefas.API/Startup.cs
@@ -27,7 +27,7 @@ namespace HerreraSoft.ControleTarefas.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World teste update!");
+                await context.Response.WriteAsync("Hello World teste update 2!");
             });
         }
     }
