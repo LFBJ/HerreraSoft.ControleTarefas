2393d42f3876780d58b97944e8e2427b5b1bc5c3
Diego Herrera <di.herrera.go@gmail.com>
2018-10-29 23:12:46 -0300

    Alteração código

--- HerreraSoft.ControleTarefas.API/Startup.cs
+++ HerreraSoft.ControleTarefas.API/Startup.cs
@@ -27,7 +27,7 @@ namespace HerreraSoft.ControleTarefas.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World teste update!");
             });
         }
     }
